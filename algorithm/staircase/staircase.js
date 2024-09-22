4cf1a7f790b5dd8053a4e26fd0c9c40a5d15ca04
Sanzary Rezvy <sanzary.html.5@gmail.com>
2024-09-23 00:32:21 +0600

    Staircase

---
+++ algorithm/staircase/staircase.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+    let result = ''
+    for(let i=1; i<=n; i++){
+        result += (' '.repeat(n - i)) + ('#'.repeat(i)) + '\n'
+    }
+    console.log(result)
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
